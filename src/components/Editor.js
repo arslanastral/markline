8e6cb0da86c1b86a77281a4d894b5cbb532bbcc1
Arslan <81504779+arslanastral@users.noreply.github.com>
2021-04-03 18:11:07 +0800

    added markdown & preview panes ✨

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Titlebar from "./Titlebar/Titlebar";
+import MarkdownPane from "./MarkdownPane";
+import PreviewPane from "./PreviewPane";
 
 const Editor = () => {
   return (
     <div className="editor-container">
       <Titlebar />
+      <div className="panes-container">
+        <MarkdownPane />
+        <PreviewPane />
+      </div>
     </div>
   );
 };
