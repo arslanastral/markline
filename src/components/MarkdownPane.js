59f0fcfaebf355ea87acdf6b574c6962aad2a1ee
Arslan <arslanastral@outlook.com>
2021-04-03 18:11:07 +0800

    added markdown & preview panes ✨

---
+++ src/components/MarkdownPane.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MarkdownPane = () => {
+  return (
+    <div className="markdown-pane">
+      <h1 className="pane-title">Markdown</h1>
+    </div>
+  );
+};
+
+export default MarkdownPane;
---
+++ src/components/PreviewPane.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PreviewPane = () => {
+  return (
+    <div className="preview-pane">
+      <h1 className="pane-title">Preview</h1>
+    </div>
+  );
+};
+
+export default PreviewPane;
