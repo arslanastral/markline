6f461e86ea7f89816b2a4e15f41c8e51d965aeef
Arslan <arslanastral@outlook.com>
2021-04-03 14:02:16 +0800

    added margins to window controls

--- src/components/Titlebar/WindowControls.js
+++ src/components/Titlebar/WindowControls.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WindowControls = () => {
   return (
-    <div>
+    <div className="window-controls">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="54"
