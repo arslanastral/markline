236b3ed43ad0d46672d14bd6921d4968e3d905b4
Arslan <81504779+arslanastral@users.noreply.github.com>
2021-04-06 17:19:42 +0800

    added styling on TextArea

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,12 +2,26 @@ import React from "react";
 import Titlebar from "./Titlebar/Titlebar";
 import Pane from "./Pane";
 
+import styled from "styled-components";
+
+const TextArea = styled.textarea`
+  width: 100%;
+  max-width: 100%;
+  background: black;
+  color: white;
+  font-family: "Inter", sans-serif;
+`;
+
 const Editor = () => {
   return (
     <div className="editor-container">
       <Titlebar />
       <div className="panes-container">
-        <Pane className="markdown-pane" title="Markdown" />
+        <Pane
+          className="markdown-pane"
+          title="Markdown"
+          textArea={<TextArea />}
+        ></Pane>
         <Pane className="preview-pane" title="Preview" />
       </div>
     </div>
