f504a2d42a1dd4b15575f5a1ec0adf466f82b04b
Arslan <arslanastral@outlook.com>
2021-04-06 17:19:42 +0800

    added styling on TextArea

--- src/components/Pane.js
+++ src/components/Pane.js
@@ -1,13 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 
-const TextArea = styled.textarea``;
-
-const Pane = ({ className, title }) => {
+const Pane = ({ className, title, textArea }) => {
   return (
     <div className={className}>
       <h1 className="pane-title">{title}</h1>
-      <TextArea />
+      {textArea}
     </div>
   );
 };
