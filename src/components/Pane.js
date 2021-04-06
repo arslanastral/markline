c8fb01ef6caecfa06f08a4de57c1e907b8aaf428
Arslan <arslanastral@outlook.com>
2021-04-06 16:51:35 +0800

    fixed config issues & added styled textarea

--- src/components/Pane.js
+++ src/components/Pane.js
@@ -1,9 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const TextArea = styled.textarea``;
 
 const Pane = ({ className, title }) => {
   return (
     <div className={className}>
       <h1 className="pane-title">{title}</h1>
+      <TextArea />
     </div>
   );
 };
