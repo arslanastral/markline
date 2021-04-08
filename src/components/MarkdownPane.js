07ff7b7f3ceea057c69cfa2c5d0893dfbe3c6cfe
Arslan <arslanastral@outlook.com>
2021-04-08 18:29:32 +0800

    fixed live markdown rendering

--- src/components/MarkdownPane.js
+++ src/components/MarkdownPane.js
@@ -1,30 +1,52 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const TextArea = styled.textarea`
-  // width: 100%;
-  background: black;
-  color: white;
-  font-family: "JetBrains Mono", monospace;
-  border-radius: 5px;
-  border: none;
-  overflow: auto;
-  outline: none;
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-markdown";
+import "ace-builds/src-noconflict/theme-twilight";
 
-  -webkit-box-shadow: none;
-  -moz-box-shadow: none;
-  box-shadow: none;
-  margin: 15px;
-`;
+// const iAceEditor = styled.AceEditor`
+//   // width: 100%;
+//   background: black;
+//   color: white;
+//   font-family: "JetBrains Mono", monospace;
+//   border-radius: 5px;
+//   border: none;
+//   overflow: auto;
+//   outline: none;
+
+//   -webkit-box-shadow: none;
+//   -moz-box-shadow: none;
+//   box-shadow: none;
+//   margin: 15px;
+// `;
 
 const MarkdownPane = ({ title, markdown, setMarkdown }) => {
   return (
     <div className="markdown-pane">
       <h1 className="pane-title">{title}</h1>
-      <TextArea
+
+      <AceEditor
+        placeholder=""
+        mode="markdown"
+        theme="twilight"
+        name="markdown-editor"
+        onChange={(e) => setMarkdown(e)}
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={markdown}
+        setOptions={{
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
+
+      {/* <TextArea
         onChange={(e) => setMarkdown(e.target.value)}
         value={markdown}
-      ></TextArea>
+      ></TextArea> */}
     </div>
   );
 };
