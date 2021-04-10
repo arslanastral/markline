751192119f2b46c55da8a1f792f88150357d345f
Arslan <arslanastral@outlook.com>
2021-04-10 20:14:11 +0800

    added dark-mode

--- src/components/Titlebar/Titlebar.js
+++ src/components/Titlebar/Titlebar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WindowControls from "./WindowControls";
+import NightMode from "./NightMode";
 
 const Titlebar = () => {
   return (
     <div className="titlebar-container">
       <WindowControls />
+      <NightMode />
     </div>
   );
 };
